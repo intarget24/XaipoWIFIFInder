42722c60f04292e045389c34abc28962cab3844c
intarget24 <intarget24@hotmai.com>
2016-09-03 13:17:13 -0500

    First Commit

---
+++ www/js/app.js
@@ -0,0 +1,70 @@
+/**
+ * Created by Intarget on 03/09/2016.
+ */
+var navigation={};
+var Myapp = {};
+Myapp.config = {
+};
+window.onload = function() {
+    navigation.mainView = navigation.fw.addView('.view-main', {
+        // Because we use fixed-through navbar we can enable dynamic navbar
+        dynamicNavbar: true,
+        domCache: true
+    });
+};
+
+navigation.fw = new Framework7();
+navigation.$$ = Dom7;
+
+Myapp.angular = angular.module('UBWifiFinder', ['chart.js']);
+Myapp.angular.controller('general', function ($scope, $http) {
+
+    $scope.ayudaTitulo = "";
+    $scope.ayudaDescripcion = "";
+    $scope.ubicodeSession = "";
+
+    $scope.fecha = "Hoy";
+    $scope.title = "Redes disponibles";
+    $scope.wifis = [];
+
+    $scope.ScanWifi = function()
+    {
+        WifiWizard.startScan(function(){
+            $scope.title = "Escaneando";
+        }, function(){
+            $scope.title = "Fallo en el escaneo";
+        });
+
+        WifiWizard.getScanResults({numLevels: 0},
+            function(networks){
+                networks.forEach(function(item){
+                    wifis.push({ssid: item.SSID , pass: 'hola'});
+                });
+                $scope.$apply();
+
+        }, function(err){
+
+        });
+
+        $scope.title = "Redes disponibles";
+    }
+
+    $scope.MenuSide= function(id)
+    {
+        switch(id)
+        {
+            case 1:
+                navigation.mainView.router.load({pageName:'wifi'});
+                break;
+            case 2:
+                navigation.mainView.router.load({pageName:'help'});
+                break;
+            case 3:
+                navigation.mainView.router.load({pageName:'about'});
+                break;
+            case 4:
+                navigator.app.exitApp();
+                break;
+        }
+    }
+});
